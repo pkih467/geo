86d55d974dd033070a6d778de850a278a08ae738
pkih467 <outsets-critic-2h@icloud.com>
2026-08-02 17:56:44 +0530

    Update console log

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/data', express.static(path.join(__dirname, 'data')));
 
 app.listen(PORT, () => {
-  console.log(`Geospatial app running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
